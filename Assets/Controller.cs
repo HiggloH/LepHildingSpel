be3cd9a793d962b6e38dbb9d02997f67ab5a48a3
HiggloH <Hilding.hammarback@gmail.com>
2019-12-28 14:10:00 +0100

    Initial commit

---
+++ Assets/Controller.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float moveSpeed = 5;
+    public float JumpSpeed = 2.5f;
+
+    public bool isGrounded = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        isGrounded = true;
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(Input.GetAxis("Horizontal") * moveSpeed, rb.velocity.y);
+
+        if(Input.GetButtonDown("Jump") && isGrounded == true)
+        {
+            rb.AddForce(-Vector2.down * JumpSpeed);
+            isGrounded = false;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "TileMap" && isGrounded == false)
+        {
+            Debug.Log("YEET");
+            isGrounded = true;
+        }
+    }
+}
