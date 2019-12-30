7d2f76fd25daa4ea7e3965d788eb280781b95dfc
HiggloH <Hilding.hammarback@gmail.com>
2019-12-30 21:36:15 +0100

    No animations and TileMap collision fixed &  EnemyController and GBController merge

--- Assets/Scripts/CameraStoper.cs
+++ Assets/Scripts/CameraStoper.cs
@@ -22,6 +22,6 @@ public class CameraStoper : MonoBehaviour
             Destroy(rb);
         }
 
-        //rb.velocity = new Vector2 (2, 0);
+        rb.velocity = new Vector2 (2, 0);
     }
 }
--- Assets/Scripts/GBController.cs
+++ Assets/Scripts/GBController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class GBController : MonoBehaviour
 {
     private GingerBreadEnemy gb;
+    
     void Start()
     {
         gb = new GingerBreadEnemy(GetComponent<Rigidbody2D>());
@@ -14,5 +15,14 @@ public class GBController : MonoBehaviour
     void Update()
     {
         gb.move("left");
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            Destroy(collision.gameObject);
+            Player.dead = true;
+        }
     }
 }
