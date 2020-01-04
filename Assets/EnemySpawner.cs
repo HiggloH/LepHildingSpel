0ac21409477e11115bc4e246594cf33ec4a22719
HiggloH <Hilding.hammarback@gmail.com>
2020-01-04 17:42:48 +0100

    Failed enemy spawner

---
+++ Assets/EnemySpawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    
+    public Transform spawner;
+    public GameObject enemy;
+    private IEnumerator timer;
+
+    private void Start()
+    {
+        timer = spawn(15f);
+        StartCoroutine(timer);
+    }
+
+    private IEnumerator spawn(float waitTime)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(waitTime);
+            Instantiate(enemy, spawner.position, spawner.rotation);
+            
+        }
+    }
+}
